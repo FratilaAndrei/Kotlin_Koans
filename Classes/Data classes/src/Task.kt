19c504380a1489a0e5236203de880e4d1de6ab0e
Fratila Andrei <andifratila121618@gmail.com>
2024-11-18 14:40:20 +0200

    Classes - Task 1 Done

--- Classes/Data classes/src/Task.kt
+++ Classes/Data classes/src/Task.kt
@@ -1,11 +1,19 @@
-class Person
+data class Person(private var name: String, private var age: Int)
 
 fun getPeople(): List<Person> {
     return listOf(Person("Alice", 29), Person("Bob", 31))
+
 }
 
 fun comparePeople(): Boolean {
-    val p1 = Person("Alice", 29)
     val p2 = Person("Alice", 29)
+    val p1 = Person("Alice", 29)
+    println(p1)
+    println(p2)
+
     return p1 == p2  // should be true
+}
+
+fun main() {
+    comparePeople()
 }
