8d4e6691ce835dff35d86739504dd5ffa3672ca9
Fratila Andrei <andifratila121618@gmail.com>
2024-11-19 15:54:59 +0200

    Collections - Filter and map

--- Collections/Filter map/src/Task.kt
+++ Collections/Filter map/src/Task.kt
@@ -1,7 +1,11 @@
 // Find all the different cities the customers are from
 fun Shop.getCustomerCities(): Set<City> =
-        TODO()
+        customers.map { it.city }.toSet()
+// we map over all customers to get their city in an array.
+// So basically we make an array of cities from customers that we set to be unique
+
+
 
 // Find the customers living in a given city
 fun Shop.getCustomersFrom(city: City): List<Customer> =
-        TODO()
+        customers.filter { it.city == city }
