d2076642578beb771a0f5b5e87b5686850348b52
Fratila Andrei <andifratila121618@gmail.com>
2024-11-20 17:30:37 +0200

    Collections - GroupBy

--- Collections/GroupBy/src/Task.kt
+++ Collections/GroupBy/src/Task.kt
@@ -1,3 +1,4 @@
 // Build a map that stores the customers living in a given city
 fun Shop.groupCustomersByCity(): Map<City, List<Customer>> =
-        TODO()
+        customers.groupBy { it.city }
+
