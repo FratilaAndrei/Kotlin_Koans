77da3f39c45a0dc873ddccd8e455b000ea931915
Fratila Andrei <andifratila121618@gmail.com>
2024-11-21 12:11:38 +0200

    Collections - Max min

--- Collections/Max min/src/Task.kt
+++ Collections/Max min/src/Task.kt
@@ -1,7 +1,21 @@
 // Return a customer who has placed the maximum amount of orders
 fun Shop.getCustomerWithMaxOrders(): Customer? =
-        TODO()
+        customers.maxByOrNull { it.orders.size }
 
 // Return the most expensive product that has been ordered by the given customer
-fun getMostExpensiveProductBy(customer: Customer): Product? =
-        TODO()
+fun getMostExpensiveProductBy(customer: Customer): Product? = customer.orders.flatMap(Order::products).maxByOrNull(Product::price)
+
+// din lista de comenzi ale unui customer, afisam produsele comandate, iar din ele cautam cel mai scump
+
+
+/*
+
+fun getMostExpensiveProductBy(customer: Customer): Product? {
+        my first variant
+    val productsList = customer.orders.flatMap(Order::products)
+    val res = productsList.maxByOrNull { it.price }
+    return res
+}
+
+
+ */
