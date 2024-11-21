94a876235e7f6e9c959e15995c920c28c17616a0
Fratila Andrei <andifratila121618@gmail.com>
2024-11-21 13:24:01 +0200

    Collections - Fold and reduce

--- Collections/Fold and reduce/src/Task.kt
+++ Collections/Fold and reduce/src/Task.kt
@@ -1,5 +1,17 @@
 // Return the set of products that were ordered by all customers
-fun Shop.getProductsOrderedByAll(): Set<Product> =TODO()
+fun Shop.getProductsOrderedByAll(): Set<Product> =
+//    customers.flatMap {it.orders}.flatMap { it.products }.filter { acc, product -> acc == product  }
+
+    customers.map { it.getOrderedProducts() }.reduce { orderedByAll, customer -> orderedByAll.intersect(customer)  }
+
+//customers.map(Customer::getOrderedProducts).reduce { orderedByAll, customer ->
+//    orderedByAll.intersect(customer)
+
+// facem rost de lista totala de produse
+// iteram prin toti customers sa aducem toate listele de comenzi cu toate listele de produse cumparata
+
+
+
 
 fun Customer.getOrderedProducts(): Set<Product> =
-    TODO()
+    orders.flatMap { it.products }.toSet()
