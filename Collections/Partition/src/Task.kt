aa47a78053529f741d52dcf1d9250dd33cfb4567
Fratila Andrei <andifratila121618@gmail.com>
2024-11-21 10:26:41 +0200

    Collections - Partition

--- Collections/Partition/src/Task.kt
+++ Collections/Partition/src/Task.kt
@@ -1,2 +1,16 @@
 // Return customers who have more undelivered orders than delivered
-fun Shop.getCustomersWithMoreUndeliveredOrders(): Set<Customer> = TODO()
+fun Shop.getCustomersWithMoreUndeliveredOrders(): Set<Customer> = customers.filter {
+    val (delivered, undelivered) = it.orders.partition { order -> order.isDelivered  }
+    delivered.count() < undelivered.count()
+}.toSet()
+
+
+/*
+    val (undeliveredOrders,deliveredOrders ) = customers.partition { it -> it.orders.count() { it.isDelivered } < it.orders.count() { !it.isDelivered } }
+    return undeliveredOrders.toSet()
+   My version
+ */
+
+
+
+
