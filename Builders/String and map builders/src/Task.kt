07c7f3bb8075fe04683dedb46a1431db1bd8ef75
Fratila Andrei <andifratila121618@gmail.com>
2024-11-22 15:39:27 +0200

    Builders - String and map builders

--- Builders/String and map builders/src/Task.kt
+++ Builders/String and map builders/src/Task.kt
@@ -1,9 +1,10 @@
 import java.util.HashMap
+import javax.swing.UIManager.put
 
-/* TODO */
+
 
 fun usage(): Map<Int, String> {
-    return buildMutableMap {
+    return buildMap {
         put(0, "0")
         for (i in 1..10) {
             put(i, "$i")
